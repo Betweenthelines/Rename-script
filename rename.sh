389dff55dec3e50744ae10a32d50ad78b0fe1631
Betweenthelines <109682700+Betweenthelines@users.noreply.github.com>
2025-04-04 23:12:13 +0530

    Update rename.sh
    I am just updating this it does 
    1. remove special character
    2. a loop to rename all files with "_.mkv" to ".mkv"
    3. another loop to replace all "_" with " ".
    
    This method makes all my anime collection compatabile to all types of storages and hard disks.

--- rename.sh
+++ rename.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 ## Rename scripts for anime or any multiple mkv files. Don't repeatedly use it as it may give unexpected results.
+# For educational purposes
+# mv "$file" "${file/'bad words'/}"
+# For meta characters, we should use ###[^]]*###
+# For example, in aa{asasas}.txt to make it aa.txt, we have to pipe it through 
+# sed -e 's/the-meta-character[^]]*the-meta-character//g'
+# The meta character is any brackets, parenthesis, etc. () {} []
+# For xfce thunar Custom action Custom command will be ""exo-open --working-directory %f --launch TerminalEmulator (path to rename.sh)""
+# I am not smart I had to add 3 loops for a perfect naming structures 
+# First loop: Clean up brackets, extra tags, replace spaces with underscores
+# Second loop: "_.mkv" ---> ".mkv"
+# third loop: "_" ---> " "
 
 for file in *.mkv
 do
     newfile=$(echo "$file" | \
-        sed -e 's/\[[^][]*\]//g' |
-        sed -e 's/([^()]*)//g' |
-        sed -e 's/\.mkv/.mkv/g' |
-        sed -e 's/{[^]]*}//g')
+        sed -e 's/\[[^][]*\]//g' \
+            -e 's/([^()]*)//g' \
+            -e 's/{[^}]*}//g')  # Fixed curly brace matching
+
     newfile="${newfile#" "}"  # Remove leading space
+
+    filename="${newfile%.*}"
+    extension="${newfile##*.}"
+    newfile="${filename// /_}.${extension}"  # Replace spaces with underscores
 
     echo "$newfile"
     mv -v "$file" "$newfile"
 
+    # Remove common tags
     mv "$newfile" "${newfile/'720p'/}"
     mv "$newfile" "${newfile/'720p'/}"
     mv "$newfile" "${newfile/'1080p'/}"
@@ -24,9 +40,17 @@ do
     # mv "$newfile" "${newfiles.mkv}"
 done
 
-# For educational purposes
-#mv "$file" "${file/'bad words'/}"
-# For meta characters, we should use ###[^]]*###
-# For example, in aa{asasas}.txt to make it aa.txt, we have to pipe it through 
-# sed -e 's/the-meta-character[^]]*the-meta-character//g'
-# The meta character is any brackets, parenthesis, etc. () {} []
+# Second loop: Fix filenames ending with '_.mkv'
+for file in *_".mkv"
+do
+    newfile="${file/_\.mkv/.mkv}"
+    mv -v "$file" "$newfile"
+done
+
+# Third loop: Replace all underscores with spaces
+for file in *.mkv
+do
+    newfile="${file//_/ }"
+    mv -v "$file" "$newfile"
+done
+	
